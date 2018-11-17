1e89c85c7997d5badd57f9e5b0cbac896a91ee69
Stefan Heine <stheine@arcor.de>
2018-11-17 15:32:06 +0100

    Make the Let's Encrypt renew process send mail on success or issues

--- qnap/certbot/renew.sh
+++ qnap/certbot/renew.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+# Note on sendmail
+# The version in /usr/sbin/sendmail uses the qnap build-in mail mechanism, while
+# the version in /share/linux/tools/busybox/sendmail uses the postfix running on port 53.
+
 cd /root/docker/
 
 RESULT=`/share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker-compose run --rm certbot /certbot/certbot-auto renew --webroot --webroot-path=/var/letsencrypt 2>/dev/null`
 
 # echo "$RESULT"
 
-if [ -n "`echo \"$RESULT\" | grep 'heine7\.de.*success'`" ]
-then
+if [ -n "`echo \"$RESULT\" | grep 'heine7\.de.*success'`" ]; then
   echo "certs updated"
   /share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker-compose kill -s HUP dovecot
   /share/CACHEDEV1_DATA/.qpkg/container-station/bin/docker-compose kill -s HUP nginx
+
+  echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate updated\n\nCertificate updated\n" | /share/linux/tools/busybox/sendmail -t
 else
   echo "$RESULT"
+
+#  if [ -n "`echo \"$RESULT\" | grep 'Cert not yet due for renewal'`" ]; then
+  if [ -n "`echo \"$RESULT\" | grep 'No renewals were attempted'`" ]; then
+    // do nothing
+  else
+    echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate update check failed\n\n$RESULT\n" | /share/linux/tools/busybox/sendmail -t
+  fi
 fi
