f4ff140d490a228cae4f4cef0720aa4656beb66b
Stefan Heine <stheine@arcor.de>
2020-12-12 09:50:10 +0100

    moment -> dayjs

--- qnap/mqtt2rrd/app/logger.js
+++ qnap/mqtt2rrd/app/logger.js
@@ -1,33 +1,36 @@
-#!/usr/bin/env node
-
 'use strict';
 
 /* eslint-disable no-console */
 
-const moment = require('moment');
+const dayjs = require('dayjs');
 
 module.exports = {
-  /* eslint-disable no-console */
+  debug(msg, params) {
+    if(params) {
+      console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} DEBUG`, msg, params);
+    } else {
+      console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} DEBUG`, msg);
+    }
+  },
   info(msg, params) {
     if(params) {
-      console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} INFO`, msg, params);
+      console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} INFO`, msg, params);
     } else {
-      console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} INFO`, msg);
+      console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} INFO`, msg);
     }
   },
   warn(msg, params) {
     if(params) {
-      console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} WARN`, msg, params);
+      console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} WARN`, msg, params);
     } else {
-      console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} WARN`, msg);
+      console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} WARN`, msg);
     }
   },
   error(msg, params) {
     if(params) {
-      console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} ERROR`, msg, params);
+      console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} ERROR`, msg, params);
     } else {
-      console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} ERROR`, msg);
+      console.log(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} ERROR`, msg);
     }
   },
-  /* eslint-enable no-console */
 };
