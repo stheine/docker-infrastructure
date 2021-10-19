748877aef0d2010023154ce57243a711bcf262f2
Stefan Heine <stheine@arcor.de>
2021-10-19 10:11:54 +0200

    Reduce number of recent backups for images

--- qnap/mediawiki-database-backup/postbackup.sh
+++ qnap/mediawiki-database-backup/postbackup.sh
@@ -3,4 +3,4 @@
 cd /mediawiki-images
 tar --exclude-vcs -zcf /backup/images/`date +%Y-%m-%d`.tgz . || exit $?
 
-find /backup/images -type f -mtime +10 -exec rm {} \;
+find /backup/images -type f -mtime +5 -exec rm {} \;
