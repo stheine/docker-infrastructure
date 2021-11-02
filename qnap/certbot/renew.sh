3e90254caf759f9bf6f72a025b69eba7357a86c8
Stefan Heine <stheine@arcor.de>
2021-11-02 21:09:39 +0100

    certbot, mount script from outside, add error handling

--- qnap/certbot/renew.sh
+++ qnap/certbot/renew.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-RESULT=`/bin/certbot renew --webroot --webroot-path=/var/letsencrypt 2>/tmp/certbot.error`
-RESULT_CODE=${?}
+SUCCESS=0
 
-# echo "${RESULT_CODE} ${RESULT}"
+until [ $SUCCESS ]; do
+  RESULT=`/bin/certbot renew --webroot --webroot-path=/var/letsencrypt 2>/tmp/certbot.error`
+  RESULT_CODE=${?}
 
-if [ ${RESULT_CODE} = 0 ]; then
-  if [ -n "`echo \"${RESULT}\" | grep 'heine7\.de.*success'`" ]; then
-    echo "certs updated"
+  # echo "${RESULT_CODE} ${RESULT}"
 
-    echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate updated\n\nCertificate updated\nTODO restart nginx and dovecot\n" | /usr/sbin/sendmail -t
-  else
-    echo "certs not updated: ${RESULT}"
+  if [ ${RESULT_CODE} = 0 ]; then
+    if [ -n "`echo \"${RESULT}\" | grep 'heine7\.de.*success'`" ]; then
+      echo "certs updated"
 
-    if [ -n "`echo \"${RESULT}\" | grep 'No renewals were attempted'`" ]; then
-      # do nothing
-      /bin/true
+      echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate updated\n\nCertificate updated\nTODO restart nginx and dovecot\n" | /usr/sbin/sendmail -t
     else
-      echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate update check failed\n\n${RESULT}\n" | /usr/sbin/sendmail -t
-    fi
-  fi
-else
-  ERROR=`cat /tmp/certbot.error`
+      echo "certs not updated: ${RESULT}"
 
-  echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate update check failed\n\n${RESULT_CODE} ${RESULT}\n${ERROR}" | /usr/sbin/sendmail -t
-fi
+      if [ -n "`echo \"${RESULT}\" | grep 'No renewals were attempted'`" ]; then
+        # do nothing
+        /bin/true
+      else
+        echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate update check failed\n\n${RESULT}\n" | /usr/sbin/sendmail -t
+      fi
+    fi
+
+    SUCCESS=1
+  else
+    ERROR=`cat /tmp/certbot.error`
+
+    echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate update check failed\n\n/bin/certbot result=${RESULT_CODE} ${RESULT}\n${ERROR}\n$(ls -l /bin)\n\n$(ls -l /opt/certbot/bin)\n\n$(ls -l /var)" | /usr/sbin/sendmail -t
+
+    sleep 10
+  fi
+done
