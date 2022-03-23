ded80b812bc378aa788c0448065f1f1b5da67104
Stefan Heine <stheine@arcor.de>
2022-03-23 10:28:54 +0100

    Fix certbot renewal

--- qnap/certbot/renew.sh
+++ qnap/certbot/renew.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-SUCCESS=0
+SUCCESS=false
 
-until [ $SUCCESS ]; do
+echo "$(date +"%Y-%m-%d %H:%M:%S") renew.sh started"
+
+until $(${SUCCESS}); do
+  echo "$(date +"%Y-%m-%d %H:%M:%S") Calling certbot renew"
+
   RESULT=`/bin/certbot renew --webroot --webroot-path=/var/letsencrypt 2>/tmp/certbot.error`
   RESULT_CODE=${?}
 
-  # echo "${RESULT_CODE} ${RESULT}"
+  echo "$(date +"%Y-%m-%d %H:%M:%S") certbot renew returned ${RESULT_CODE} ${RESULT}"
 
   if [ ${RESULT_CODE} = 0 ]; then
     if [ -n "`echo \"${RESULT}\" | grep 'heine7\.de.*success'`" ]; then
-      echo "certs updated"
+      echo "$(date +"%Y-%m-%d %H:%M:%S") certs updated"
 
-      echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate updated\n\nCertificate updated\nTODO restart nginx and dovecot\n" | /usr/sbin/sendmail -t
+      echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate updated\n\nCertificate updated\nTODO restart nginx and dovecot\n\ncd docker\ndocker-compose restart nginx dovecot" | /usr/sbin/sendmail -t
     else
-      echo "certs not updated: ${RESULT}"
+      echo "$(date +"%Y-%m-%d %H:%M:%S") certs not updated: ${RESULT}"
 
       if [ -n "`echo \"${RESULT}\" | grep 'No renewals were attempted'`" ]; then
         # do nothing
@@ -24,12 +28,16 @@ until [ $SUCCESS ]; do
       fi
     fi
 
-    SUCCESS=1
+    SUCCESS=true
   else
     ERROR=`cat /tmp/certbot.error`
+
+    echo "$(date +"%Y-%m-%d %H:%M:%S") certbot renew failed: ${RESULT_CODE} ${RESULT} ${ERROR}" >&2
 
     echo -e "From: technik@heine7.de\nTo: technik@heine7.de\nSubject: Certificate update check failed\n\n/bin/certbot result=${RESULT_CODE} ${RESULT}\n${ERROR}\n$(ls -l /bin)\n\n$(ls -l /opt/certbot/bin)\n\n$(ls -l /var)" | /usr/sbin/sendmail -t
 
     sleep 10
   fi
 done
+
+echo "$(date +"%Y-%m-%d %H:%M:%S") renew.sh finished"
