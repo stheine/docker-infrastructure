54d0154923d6a35c376fe1ac1ff6f99b225981af
Stefan Heine <stheine@arcor.de>
2023-02-22 11:04:50 +0100

    Add more containers to log view

--- qnap/log2.sh
+++ qnap/log2.sh
@@ -1,1 +1,1 @@
-docker-compose logs --tail 15 --follow fritz mqtt-strom mqtt-vito mqtt2rrd watchdog
+docker-compose logs --tail 15 --follow fritz mqtt-strom mqtt-vito mqtt2rrd watchdog mqtt-volumio mqtt-wetter
