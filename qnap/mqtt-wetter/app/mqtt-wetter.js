edff7c919eef0fc99acb76b7c5605fcbefb570f9
Stefan Heine <stheine@arcor.de>
2023-03-31 08:15:07 +0200

    wetter, add handling for DWD weather source

--- qnap/mqtt-wetter/app/mqtt-wetter.js
+++ qnap/mqtt-wetter/app/mqtt-wetter.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import _          from 'lodash';
+import axios      from 'axios';
+import check      from 'check-types-2';
 import mqtt       from 'async-mqtt';
 import ms         from 'ms';
 
@@ -32,6 +34,182 @@ const stopProcess = async function() {
 };
 
 process.on('SIGTERM', () => stopProcess());
+
+const handleWeatherDWD = async function() {
+  const {eveningStartsHour, morningEndsHour, dwdStationId} = config;
+
+  // Timestamp calculation, local time to UTC
+  // - now hours
+  const date = new Date();
+
+  const nowUTCHour = date.getUTCHours();
+
+  // - evening starts
+  date.setHours(eveningStartsHour);
+
+  const eveningStartsUTCHour = date.getUTCHours();
+
+  // morning ends
+  date.setHours(morningEndsHour);
+
+  const morningEndsUTCHour = date.getUTCHours();
+
+
+
+
+  const nextEvening = new Date();
+  const nextMorning = new Date();
+
+  if(nextEvening.getUTCHours() > eveningStartsUTCHour) {
+    // after evening starts, before midnight. next evening is tomorrow.
+    nextEvening.setUTCDate(nextEvening.getUTCDate() + 1);
+  }
+  nextEvening.setUTCHours(eveningStartsUTCHour);
+  nextEvening.setUTCMinutes(0);
+
+  if(nextMorning.getUTCHours() > morningEndsUTCHour) {
+    // after morning ends. next morning is tomorrow.
+    nextMorning.setUTCDate(nextMorning.getUTCDate() + 1);
+  }
+  nextMorning.setUTCHours(morningEndsUTCHour);
+  nextMorning.setUTCMinutes(0);
+
+
+  logger.debug('Timestamps', {
+    local:       {eveningStartsHour, morningEndsHour},
+    utc:         {nowUTCHour, eveningStartsUTCHour, morningEndsUTCHour},
+    nextMorning,
+    nextEvening,
+  });
+
+  // Get weather data
+  let results;
+
+  try {
+    results = await Promise.all([
+      (async() => {
+        // Current
+        const host = 'api.open-meteo.com';
+        const base = '/v1/dwd-icon';
+        const url  = `https://${host}${base}?latitude=48.6207&longitude=8.8988&current_weather=true`; // TODO location config
+        const result = await await axios.get(url, {timeout: ms('2 seconds')});
+
+        check.assert.object(result, `result not an object`);
+        check.assert.object(result.data, `result.data not an object`);
+        check.assert.object(result.data.current_weather, `result.data.current_weather not an object`);
+        check.assert.equal(result.status, 200, `Unexpected status=${result.status}`);
+
+        return result.data.current_weather;
+      })(),
+      (async() => {
+        // Forecast
+        const host = 'app-prod-ws.warnwetter.de';
+        const base = '/v30/stationOverviewExtended';
+        const url  = `https://${host}${base}?stationIds=${encodeURIComponent(dwdStationId)}`;
+        const result = await await axios.get(url, {timeout: ms('2 seconds')});
+
+        check.assert.object(result, `result not an object`);
+        check.assert.object(result.data, `result.data not an object`);
+        check.assert.object(result.data[dwdStationId], `result.data[dwdStationId] not an object`);
+        check.assert.equal(result.status, 200, `Unexpected status=${result.status}`);
+
+        return result.data[dwdStationId];
+      })(),
+    ]);
+  } catch(err) {
+    logger.error(`Weather: Failed to get '${err.message}'`);
+  }
+
+  const current  = results[0];
+  const forecast = results[1];
+
+  // K2920
+  //   forecast1
+  //     stationId                      "K2920"
+  //     start                          1678402800000
+  //     timeStep                       3600000
+  //     temperature                    […]
+  //     temperatureStd                 […]
+  //     windSpeed                      null
+  //     windDirection                  null
+  //     windGust                       null
+  //     icon                           […]
+  //     precipitationTotal             […]
+  //     dewPoint2m                     […]
+  //     surfacePressure                […]
+  //     humidity                       […]
+  //     isDay                          […]
+  //     precipitationProbablity        null
+  //     precipitationProbablityIndex   null
+  //   forecast2                        {…}
+  //   forecastStart                    null
+  //   days
+  //     0                              {…}
+  //     ...
+  //     9                              {…}
+  //   warnings                         […]
+  //   threeHourSummaries               null
+
+//  let dayHourly;
+//  let nightHourly;
+
+  if(nextEvening < nextMorning) {
+    // Day. rest of today and coming night.
+//     dayHourly   = weather.hourly.filter(set => set.dt * 1000 < nextEvening);
+//     nightHourly = weather.hourly.filter(set => set.dt * 1000 >= nextEvening && set.dt * 1000 < nextMorning);
+  } else {
+    // nextEvening > nextMorning
+    // Night. rest of night and coming day.
+
+// // TODO ????
+// //    const tomorrowEvening = new Date(nextEvening);
+// //
+// //    if(tomorrowEvening.getUTCHours() > morningEndsUTCHour) {
+// //      // night. evening. coming day is tomorrow.
+// //      tomorrowEvening.setUTCDate(tomorrowEvening.getUTCDate() + 1);
+// //    }
+// //    tomorrowEvening.setUTCHours(eveningStartsUTCHour);
+// //    tomorrowEvening.setUTCMinutes(0);
+// //    dayHourly   = weather.hourly.filter(set => set.dt * 1000 >= nextMorning && set.dt * 1000 < tomorrowEvening);
+//
+//     dayHourly   = weather.hourly.filter(set => set.dt * 1000 >= nextMorning && set.dt * 1000 < nextEvening);
+//     nightHourly = weather.hourly.filter(set => set.dt * 1000 < nextMorning);
+  }
+
+//  const dayMaxTemp   = Math.max(...dayHourly.map(set => set.temp));
+//  const dayMinTemp   = Math.min(...dayHourly.map(set => set.temp));
+//  const dayMaxWind   = Math.max(...dayHourly.map(set => set.wind_speed));
+//  const nightMaxTemp = Math.max(...nightHourly.map(set => set.temp));
+//  const nightMinTemp = Math.min(...nightHourly.map(set => set.temp));
+//  const nightMaxWind = Math.max(...nightHourly.map(set => set.wind_speed));
+
+  // console.log(Night.map(set => new Date(set.dt * 1000).toISOString()));
+  // console.log({DayMaxWind, DayMinTemp, DayMaxTemp, NightMaxWind, NightMinTemp, NightMaxTemp});
+
+//  logger.debug('Day', {
+//    first: new Date(dayHourly.at(0).dt  * 1000).toISOString(),
+//    last:  new Date(dayHourly.at(-1).dt * 1000).toISOString(),
+//    temp:  _.map(dayHourly, 'temp'),
+//  });
+//  logger.debug('Night', {
+//    first: new Date(nightHourly.at(0).dt  * 1000).toISOString(),
+//    last:  new Date(nightHourly.at(-1).dt * 1000).toISOString(),
+//    temp:  _.map(nightHourly, 'temp'),
+//  });
+
+  await mqttClient.publish('wetter/dwd/INFO', JSON.stringify({
+    current,
+    forecast,
+//    dayMaxWind,
+//    dayMinTemp,
+//    dayMaxTemp,
+    eveningStartsHour,
+    morningEndsHour,
+//    nightMaxWind,
+//    nightMinTemp,
+//    nightMaxTemp,
+  }), {retain: true});
+};
 
 const handleWeather = async function() {
   const {eveningStartsHour, morningEndsHour, openWeatherLocation, suncalcLocation} = config;
@@ -143,7 +321,7 @@ const handleWeather = async function() {
     temp:  _.map(nightHourly, 'temp'),
   });
 
-  await mqttClient.publish('wetter/INFO', JSON.stringify({
+  await mqttClient.publish('wetter/openweather/INFO', JSON.stringify({
     ...weather,
     dayMaxWind,
     dayMinTemp,
@@ -187,8 +365,9 @@ const handleSunTimes = async function() {
   mqttClient.on('end',        ()  => _.noop() /* logger.info('mqtt.end') */);
 
   setInterval(handleWeather, ms('1 hour'));
-
   setInterval(handleSunTimes, ms('4 hours'));
-
   handleWeather(); // on startup
+
+  setInterval(handleWeatherDWD, ms('1 hour'));
+  handleWeatherDWD(); // on startup
 })();
