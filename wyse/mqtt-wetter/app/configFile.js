302038438e1dfa75919216b028287a0b57c0a37c
Stefan Heine <stheine@arcor.de>
2024-04-27 16:08:48 +0000

    mqtt-wetter, check various places of the config file for in-docker or direct call

--- wyse/mqtt-wetter/app/configFile.js
+++ wyse/mqtt-wetter/app/configFile.js
@@ -1,15 +1,33 @@
 import fsExtra from 'fs-extra';
 
-const configFile = '/var/wetter/config.json';
+const configFiles = [
+  '/var/wetter/config.json',
+  '/home/stheine/data/wetter/config.json',
+];
+let activeConfigFile;
 
 export default {
   async read() {
-    const config = await fsExtra.readJson(configFile);
+    let config;
+
+    for(const configFile of [activeConfigFile, ...configFiles]) {
+      try {
+        config = await fsExtra.readJson(configFile);
+
+        activeConfigFile = configFile;
+      } catch{
+        // ignore
+      }
+    }
 
     return config;
   },
 
   async write(config) {
-    await fsExtra.writeJson(configFile, config);
+    if(!activeConfigFile) {
+      throw new Error('No active config file detected');
+    }
+
+    await fsExtra.writeJson(activeConfigFile, config);
   },
 };
