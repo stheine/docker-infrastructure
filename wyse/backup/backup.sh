d95ec65c085bff95c4e1f7d82d6e42ba55bceebb
Stefan Heine <stheine@arcor.de>
2024-04-28 18:35:52 +0000

    Add backup service (currently for paperless)

---
+++ wyse/backup/backup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+mkdir -p /backup
+
+echo "$(date +"%Y-%m-%d %H:%M:%S") Backup start" | tee -a /backup.log
+
+/usr/local/bin/docker exec docker-paperless-1 document_exporter /export
+
+cd /data
+
+for dir in $(ls -1); do
+  echo "$(date +"%Y-%m-%d %H:%M:%S") Backup start: $(dir)" | tee -a /backup.log
+
+  tar -zcf /backup/$(dir)_$(date +%Y-%m-%d).tgz . || exit $?
+
+  echo "$(date +"%Y-%m-%d %H:%M:%S") Backup finished: $(dir)" | tee -a /backup.log
+done
+
+echo "$(date +"%Y-%m-%d %H:%M:%S") Cloud Copy start" | tee -a /backup.log
+
+find /backup -type f -mtime +5 -exec rm {} \;
+
+rclone copy /backup pcloud:backup
+
+echo "$(date +"%Y-%m-%d %H:%M:%S") Cloud Copy finished" | tee -a /backup.log
