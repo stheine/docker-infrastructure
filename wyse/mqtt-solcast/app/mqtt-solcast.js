13c5cbdac3282732b4e200dc5951786c11fae270
Stefan Heine <stheine@arcor.de>
2025-01-09 19:50:01 +0100

    mqtt-solcast, update, refactor, provide analyzed data

--- wyse/mqtt-solcast/app/mqtt-solcast.js
+++ wyse/mqtt-solcast/app/mqtt-solcast.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 
-import os       from 'node:os';
+import os         from 'node:os';
 
-import _        from 'lodash';
-import {logger} from '@stheine/helpers';
-import mqtt     from 'mqtt';
-import ms       from 'ms';
+import _          from 'lodash';
+import {Cron}     from 'croner';
+import {logger}   from '@stheine/helpers';
+import mqtt       from 'mqtt';
+import ms         from 'ms';
 
-import configFile            from './configFile.js';
-import {getSolcastForecasts} from './solcast.js';
+import configFile from './configFile.js';
+import {
+  analyzeForecasts,
+  getSolcastForecasts,
+} from './solcast.js';
 
 // ###########################################################################
 // Globals
@@ -47,6 +51,10 @@ const handleSolcast = async function() {
 
     await mqttClient.publishAsync('solcast/forecasts', JSON.stringify(forecasts), {retain: true});
 
+    const analysis = analyzeForecasts(forecasts);
+
+    await mqttClient.publishAsync('solcast/analysis', JSON.stringify(analysis), {retain: true});
+
     status = 'OK';
   } catch(err) {
     logger.error(err.message);
@@ -55,37 +63,46 @@ const handleSolcast = async function() {
   }
 };
 
-(async() => {
-  // #########################################################################
-  // Config
-  config = await configFile.read();
+// #########################################################################
+// Config
+config = await configFile.read();
 
-  // #########################################################################
-  // Startup
-  logger.info(`Startup --------------------------------------------------`);
+// #########################################################################
+// Startup
+logger.info(`Startup --------------------------------------------------`);
 
-  // #########################################################################
-  // Init MQTT
-  mqttClient = await mqtt.connectAsync('tcp://192.168.6.5:1883', {clientId: hostname});
+// #########################################################################
+// Init MQTT
+mqttClient = await mqtt.connectAsync('tcp://192.168.6.5:1883', {clientId: hostname});
 
-  // #########################################################################
-  // Register MQTT events
+// #########################################################################
+// Register MQTT events
 
-  mqttClient.on('connect',    ()  => logger.info('mqtt.connect'));
-  mqttClient.on('reconnect',  ()  => logger.info('mqtt.reconnect'));
-  mqttClient.on('close',      ()  => _.noop() /* logger.info('mqtt.close') */);
-  mqttClient.on('disconnect', ()  => logger.info('mqtt.disconnect'));
-  mqttClient.on('offline',    ()  => logger.info('mqtt.offline'));
-  mqttClient.on('error',      err => logger.info('mqtt.error', err));
-  mqttClient.on('end',        ()  => _.noop() /* logger.info('mqtt.end') */);
+mqttClient.on('connect',    ()  => logger.info('mqtt.connect'));
+mqttClient.on('reconnect',  ()  => logger.info('mqtt.reconnect'));
+mqttClient.on('close',      ()  => _.noop() /* logger.info('mqtt.close') */);
+mqttClient.on('disconnect', ()  => logger.info('mqtt.disconnect'));
+mqttClient.on('offline',    ()  => logger.info('mqtt.offline'));
+mqttClient.on('error',      err => logger.info('mqtt.error', err));
+mqttClient.on('end',        ()  => _.noop() /* logger.info('mqtt.end') */);
 
-  healthInterval = setInterval(async() => {
-    await mqttClient.publishAsync(`mqtt-solcast/health/STATE`, status);
-  }, ms('1 minute'));
+// #########################################################################
+// Schedule tasks
 
-  setInterval(handleSolcast, ms('30 minutes'));
+setInterval(handleSolcast, ms('1 hour'));
 
-  // Trigger once on startup
+//                    s m h d m wd
+const job = new Cron('0 0 0 * * *', {timezone: 'UTC'}, async() => {
   await handleSolcast();
+});
+
+_.noop('Cron job started', job);
+
+await handleSolcast();
+
+// #########################################################################
+// Health
+healthInterval = setInterval(async() => {
   await mqttClient.publishAsync(`mqtt-solcast/health/STATE`, status);
-})();
+}, ms('1 minute'));
+await mqttClient.publishAsync(`mqtt-solcast/health/STATE`, status);
--- wyse/mqtt-solcast/app/solcast.js
+++ wyse/mqtt-solcast/app/solcast.js
@@ -1,10 +1,136 @@
 import fsPromises from 'node:fs/promises';
 
+import _          from 'lodash';
 import axios      from 'axios';
 import check      from 'check-types-2';
+import dayjs      from 'dayjs';
 import fsExtra    from 'fs-extra';
 import {logger}   from '@stheine/helpers';
 import ms         from 'ms';
+import utc        from 'dayjs/plugin/utc.js';
+
+dayjs.extend(utc);
+
+const dcLimit = 6200; // TODO config
+
+export const analyzeForecasts = function(forecasts) {
+  const now                  = dayjs.utc();
+  const midnightTime         = now.clone().hour(24).minute(0).second(0);
+  const tomorrowMidnightTime = now.clone().hour(48).minute(0).second(0);
+
+  const hourlyForecasts = [];
+  let   lastEstimateWh;
+  let   lastStartDate;
+  let   highPvHours     = 0;
+  let   highPvWh        = 0;
+  let   limitPvHours    = 0;
+  let   limitPvWh       = 0;
+  let   tomorrowPvWh    = 0;
+  let   totalPvHours    = 0;
+  let   totalPvWh       = 0;
+
+  // Note, the pv_estimate is given in kWh. Multiply 1000 to get Wh.
+  for(const forecast of forecasts) {
+    const estimateWh = _.round(forecast.pv_estimate * 1000 / 2); // kWh to Wh, for 30min
+
+    if(!estimateWh) {
+      continue;
+    }
+
+    const periodEndDate   = dayjs(forecast.period_end);
+    const periodStartDate = periodEndDate.subtract(30, 'minutes'); // period length is 30min
+
+    if(periodStartDate < now) {
+      continue;
+    }
+
+    // console.log(periodStartDate);
+
+    if(periodStartDate.minute() === 0) {
+      if(lastStartDate) {
+        hourlyForecasts.push({
+          estimateWh: lastEstimateWh,
+          startDate:  lastStartDate,
+          timeH:      Number(dayjs(lastStartDate).format('H')),
+          timeUtcH:   Number(dayjs.utc(lastStartDate).format('H')),
+        });
+      }
+
+      lastEstimateWh = estimateWh;
+      lastStartDate  = periodStartDate;
+    } else {
+      const previousPeriodStartDate = periodStartDate.subtract(30, 'minutes');
+
+      if(!lastStartDate) {
+        lastEstimateWh = estimateWh;
+        lastStartDate  = previousPeriodStartDate;
+      } else if(lastStartDate.hour() !== previousPeriodStartDate.hour()) {
+        hourlyForecasts.push({
+          estimateWh: lastEstimateWh,
+          startDate:  lastStartDate,
+          timeH:      Number(dayjs(lastStartDate).format('H')),
+          timeUtcH:   Number(dayjs.utc(lastStartDate).format('H')),
+        });
+
+        lastEstimateWh = estimateWh;
+        lastStartDate  = previousPeriodStartDate;
+      } else {
+        lastEstimateWh += estimateWh;
+      }
+    }
+
+    switch(true) {
+      case periodEndDate < now:
+        // Already passed
+        break;
+
+      case periodEndDate < midnightTime:
+        // Today
+        if(estimateWh) {
+          totalPvHours += 0.5; // For 30min period
+          totalPvWh    += estimateWh;
+        }
+        if(estimateWh > 1500) {
+          highPvHours += 0.5; // For 30min period
+          highPvWh    += estimateWh;
+        }
+        if(estimateWh > dcLimit / 2) {
+          // Estimate is for 30 minute period
+          limitPvHours += 0.5; // For 30min period
+          limitPvWh    += estimateWh;
+        }
+        break;
+
+      case periodEndDate < tomorrowMidnightTime:
+        // Tomorrow
+        tomorrowPvWh += estimateWh;
+        break;
+
+      default:
+        // After tomorrow
+        break;
+    }
+  }
+  hourlyForecasts.push({
+    estimateWh: lastEstimateWh,
+    startDate:  lastStartDate,
+    timeH:      Number(dayjs(lastStartDate).format('H')),
+    timeUtcH:   Number(dayjs.utc(lastStartDate).format('H')),
+  });
+
+  // console.log('SolarForecast', {hourlyForecasts});
+
+  return {
+    highPvHours,
+    highPvWh,
+    hourlyForecasts,
+    limitPvHours,
+    limitPvWh,
+    tomorrowPvWh,
+    totalPvHours,
+    totalPvWh,
+  };
+};
 
 export const getSolcastForecasts = async function(config) {
   let cacheAge;
@@ -39,12 +165,12 @@ export const getSolcastForecasts = async function(config) {
     // logger.info('Refresh solcast cache');
 
     const response = await axios.get(
-      `https://api.solcast.com.au/rooftop_sites/${config.RESOURCE_ID}/forecasts?hours=36`,
+      `https://api.solcast.com.au/rooftop_sites/${config.RESOURCE_ID}/forecasts`,
       {
         headers:        {Authorization: `Bearer ${config.API_KEY}`},
         json:           true,
         validateStatus: null,
-      },
+      }
     );
 
     check.assert.nonEmptyObject(response);
@@ -58,7 +184,7 @@ export const getSolcastForecasts = async function(config) {
 
     await fsExtra.writeJson('/var/solcast/solcast-cache.json', newSolcast, {spaces: 2});
 
-    // logger.info('Refreshed solcast cache');
+    logger.trace('Refreshed solcast cache');
 
     return newSolcast.forecasts;
   } catch(err) {
