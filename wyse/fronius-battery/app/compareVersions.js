d73a0d73c6962d801761bf564003a0665dee1fac
Stefan Heine <stheine@arcor.de>
2025-12-17 19:15:41 +0100

    fronius-battery, fix version check

--- wyse/fronius-battery/app/compareVersions.js
+++ wyse/fronius-battery/app/compareVersions.js
@@ -6,17 +6,15 @@ import FroniusClient    from './fronius-client.js';
 import getLatestVersion from './getLatestVersion.js';
 import sunspec          from './sunspec_map_inverter.js';
 
-(async() => {
-  const inverter = new FroniusClient({ip: '192.168.6.11', port: 502, id: 1, sunspec});
+const inverter = new FroniusClient({ip: '192.168.6.11', port: 502, id: 1, sunspec});
 
-  try {
-    await inverter.open();
+try {
+  await inverter.open();
 
-    const currentVersion = await inverter.readRegister('Vr');
-    const latestVersion  = await getLatestVersion();
+  const currentVersion = await inverter.readRegister('Vr');
+  const latestVersion  = await getLatestVersion();
 
-    logger.info({currentVersion, latestVersion});
-  } finally {
-    await inverter.close();
-  }
-})();
+  logger.info({currentVersion, latestVersion});
+} finally {
+  await inverter.close();
+}
